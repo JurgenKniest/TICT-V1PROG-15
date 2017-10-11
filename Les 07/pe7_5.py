7bb3e400d03302af91de7cdbfad4082814d71cd9
JurgenKniest <jurgen.kniest@student.hu.nl>
2017-10-11 13:36:43 +0200

    T/m Final assignment 10

---
+++ Les 07/pe7_5.py
@@ -0,0 +1,20 @@
+def namen():
+    #lege lijst aanmaken
+    namenlijst = {}
+    while True:
+        #naam invoeren:
+        naam = input("Volgende naam: ")
+        #als de naam al in de lijst zit, tel er een bij op
+        if naam in namenlijst.keys():
+            namenlijst[naam] += 1
+        #als de naam nog niet in de lijst staat en hij leeg is, stop het proces.
+        elif naam == 'stop':
+            break
+        #als de naam er nog niet in zit en niet leeg is, voeg de naam toe met de waarde 1
+        else:
+            namenlijst[naam] = 1
+    #roep de lijst aan
+    return namenlijst
+
+#print de functie
+print(namen())
